e9da44a246a87d25c7f947ef8f086d015bcec7e0
Radosław Panuszewski <radoslaw.panuszewski15@gmail.com>
2020-11-11 18:05:24 +0100

    Add missing include_directories config

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <utility>
+#include <boost/graph/graph_traits.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/graph/topological_sort.hpp>
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
