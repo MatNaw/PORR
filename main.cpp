614081d01ed66c55bcc5369b5003f425f7209c6b
Radosław Panuszewski <radoslaw.panuszewski15@gmail.com>
2020-11-11 18:21:14 +0100

    Boost adjacency_matrix example

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include <iostream>
-#include <utility>
 #include <boost/graph/graph_traits.hpp>
-#include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/topological_sort.hpp>
+#include <boost/graph/adjacency_matrix.hpp>
+#include <boost/graph/graph_utility.hpp>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    enum { A, B, C, D, E, F, N };
+    const char* name = "ABCDEF";
+
+    typedef boost::adjacency_matrix<boost::directedS> Graph;
+    Graph g(N);
+    add_edge(B, C, g);
+    add_edge(B, F, g);
+    add_edge(C, A, g);
+    add_edge(C, C, g);
+    add_edge(D, E, g);
+    add_edge(E, D, g);
+    add_edge(F, A, g);
+
+    std::cout << "vertex set: ";
+    boost::print_vertices(g, name);
+    std::cout << std::endl;
+
+    std::cout << "edge set: ";
+    boost::print_edges(g, name);
+    std::cout << std::endl;
+
+    std::cout << "out-edges: " << std::endl;
+    boost::print_graph(g, name);
+    std::cout << std::endl;
 }
